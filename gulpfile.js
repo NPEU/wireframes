73a10f9d190be723d5d14457b78c2d7c0fa63e2b
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2018-02-07 16:47:07 +0000

    Hmm. Not quite right

--- gulpfile.js
+++ gulpfile.js
@@ -118,7 +118,8 @@ gulp.task('concat_js', () =>
         './_scripts/cookie-notice-settings.js',
         './_scripts/layout-adjustments.js',
         //'./bower_components/Fall-Back-Nav-Bar/nav-bar.js',
-        //'./bower_components/Fall-Back-Over-Panel/over-panel.js',
+        './bower_components/Fall-Back-Dropdown/dropdown.js',
+        './bower_components/Fall-Back-Over-Panel/over-panel.js',
         './bower_components/Fall-Back-Cookie-Notice/cookie-notice.js',
         './bower_components/Fall-Back-SVG/svg.js'
     ])
