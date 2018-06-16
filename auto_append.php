08847b5ab3f393175660a19cab4ae00746a09109
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2018-06-16 16:51:48 +0100

    Update base

--- auto_append.php
+++ auto_append.php
@@ -2,7 +2,7 @@
 $output = ob_get_contents();
 ob_end_clean();
 
-$output = str_replace('http://localhost:4001/', 'https://lab.npeu.ox.ac.uk/mockup/', $output);
+$output = str_replace('http://localhost:4001/', 'https://lab.npeu.ox.ac.uk/wireframe/', $output);
 
 echo $output;
 ?>
